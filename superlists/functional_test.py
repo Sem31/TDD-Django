0360c0af69c466e6f2744d4cb62bf92f4cd3dbeb
Sem31 <31kamleshprajapat@gmail.com>
2019-12-12 15:17:38 +0530

    add class in functioanl_test.py and setup the file.

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
 path = "/home/abc/Downloads/geckodriver"
-browser = webdriver.Firefox(executable_path =path)
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox(executable_path =path)
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_start_list_and_retrieve_it_later(self):
+		#to check it home page
+		self.browser.get("http://localhost:8000")
+
+		self.assertIn('To-Do',self.browser.title)
+		self.fail('Finsh the test!')
+
+
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
